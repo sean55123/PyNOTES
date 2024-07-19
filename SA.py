31119c6f99d3916bb7ad8f08b39a1e948be5f483
sean55123 <sean880316@gmail.com>
2024-07-20 00:31:35 +0800

    SA early stop

--- SA.py
+++ SA.py
@@ -42,11 +42,21 @@ def optimize(n_iterations, xMax, xMin, decimal, objective_function, model, csvfi
     wbpath += '.csv'
     accept_count = 0
     current_run = 0
+    early_stop = 0
     model.preheating(objective_function)
     while model.T > model.Tf:
+        if early_stop == 1:
+            break
         for i in range(n_iterations):
             model.fit(decimal, xMax, xMin)
             score = objective_function(model.X_next)
+            
+            if i != 0 and i%5 == 0:
+                if model.score == score:
+                    early_stop += 1
+                if early_stop == 1:
+                    break
+            
             if score < model.score:
                 model.score = score
                 model.X_init = model.X_next.copy()
